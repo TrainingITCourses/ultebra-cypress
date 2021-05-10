8cc3dd60b69a17120326c341c0129021d8f74d1f
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-10 18:28:14 +0200

    test: 11

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addTask', inputTaskDescription => {
+  const selectorFormInput = 'form > input';
+  const selectorFormButton = 'form > button';
+  const inputButtonText = 'Add task';
+  cy.get(selectorFormInput).type(inputTaskDescription);
+  cy.get(selectorFormButton).contains(inputButtonText).click();
+});
+
+Cypress.Commands.add('deleteTasks', () => {
+  const selectorDeleteButtons = 'button.delete';
+  cy.get(selectorDeleteButtons).each($el => $el.click());
+});
